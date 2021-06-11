06574c921f8b1d841c0252f1f6b2b937cf160718
Viola Ngugi <Viola_Ngugi@cable.comcast.com>
2021-06-11 14:08:20 -0400

    Assignment 5

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Write your JavaScript code here!
-
+window.addEventListener("load", function() {
+   let form = document.querySelector("launchForm");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotInput = document.querySelector("input[name=pilotName]");
+      let copilotInput = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      
+      if (pilotName.value === ""|| copilotName.value ===""|| fuelLevel.value ===""|| cargoMass.value ==="") {
+         alert("All fields required!")
+      } 
+   });
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
